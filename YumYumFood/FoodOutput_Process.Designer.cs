6b08d3fa8fa364fcac53d65ecb9f42269c1a2aa9
yoj3289 <james3289@naver.com>
2024-12-02 00:27:16 +0900

    폰트변경

--- YumYumFood/FoodOutput_Process.Designer.cs
+++ YumYumFood/FoodOutput_Process.Designer.cs
@@ -46,11 +46,10 @@
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button2.Location = new System.Drawing.Point(611, 342);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(428, 228);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(230, 60);
+            this.button2.Size = new System.Drawing.Size(161, 40);
             this.button2.TabIndex = 25;
             this.button2.Text = "취소";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,11 +57,10 @@
             // 
             // btnOk
             // 
-            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnOk.Location = new System.Drawing.Point(334, 342);
-            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOk.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(234, 228);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(230, 60);
+            this.btnOk.Size = new System.Drawing.Size(161, 40);
             this.btnOk.TabIndex = 24;
             this.btnOk.Text = "확인";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -71,12 +69,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 435);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 290);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1014, 248);
+            this.dataGridView1.Size = new System.Drawing.Size(710, 165);
             this.dataGridView1.TabIndex = 23;
             // 
             // foodOutDate
@@ -84,94 +81,85 @@
             this.foodOutDate.BackColor = System.Drawing.SystemColors.Window;
             this.foodOutDate.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.foodOutDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.foodOutDate.Location = new System.Drawing.Point(284, 272);
-            this.foodOutDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foodOutDate.Location = new System.Drawing.Point(199, 181);
             this.foodOutDate.Name = "foodOutDate";
             this.foodOutDate.ReadOnly = true;
-            this.foodOutDate.Size = new System.Drawing.Size(684, 43);
+            this.foodOutDate.Size = new System.Drawing.Size(480, 31);
             this.foodOutDate.TabIndex = 22;
             // 
             // foodPrice
             // 
             this.foodPrice.Cursor = System.Windows.Forms.Cursors.Default;
             this.foodPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.foodPrice.Location = new System.Drawing.Point(284, 210);
-            this.foodPrice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foodPrice.Location = new System.Drawing.Point(199, 140);
             this.foodPrice.Name = "foodPrice";
-            this.foodPrice.Size = new System.Drawing.Size(684, 43);
+            this.foodPrice.Size = new System.Drawing.Size(480, 31);
             this.foodPrice.TabIndex = 21;
             // 
             // foodQuantity
             // 
             this.foodQuantity.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.foodQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.foodQuantity.Location = new System.Drawing.Point(284, 148);
-            this.foodQuantity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foodQuantity.Location = new System.Drawing.Point(199, 99);
             this.foodQuantity.Name = "foodQuantity";
-            this.foodQuantity.Size = new System.Drawing.Size(684, 43);
+            this.foodQuantity.Size = new System.Drawing.Size(480, 31);
             this.foodQuantity.TabIndex = 20;
             // 
             // foodCode
             // 
             this.foodCode.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.foodCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.foodCode.Location = new System.Drawing.Point(284, 88);
-            this.foodCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foodCode.Location = new System.Drawing.Point(199, 59);
             this.foodCode.Name = "foodCode";
-            this.foodCode.Size = new System.Drawing.Size(684, 43);
+            this.foodCode.Size = new System.Drawing.Size(480, 31);
             this.foodCode.TabIndex = 19;
             // 
             // foodOutput
             // 
             this.foodOutput.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.foodOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.foodOutput.Location = new System.Drawing.Point(284, 27);
-            this.foodOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foodOutput.Location = new System.Drawing.Point(199, 18);
             this.foodOutput.Name = "foodOutput";
-            this.foodOutput.Size = new System.Drawing.Size(684, 43);
+            this.foodOutput.Size = new System.Drawing.Size(480, 31);
             this.foodOutput.TabIndex = 18;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(150, 268);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(105, 179);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 47);
+            this.label5.Size = new System.Drawing.Size(63, 32);
             this.label5.TabIndex = 17;
             this.label5.Text = "날짜";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(81, 207);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(57, 138);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(148, 47);
+            this.label4.Size = new System.Drawing.Size(111, 32);
             this.label4.TabIndex = 16;
             this.label4.Text = "출고단가";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(150, 146);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(105, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 47);
+            this.label3.Size = new System.Drawing.Size(63, 32);
             this.label3.TabIndex = 15;
             this.label3.Text = "수량";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(47, 86);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 47);
+            this.label2.Size = new System.Drawing.Size(135, 32);
             this.label2.TabIndex = 14;
             this.label2.Text = "식자재번호";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -179,20 +167,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(116, 24);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("세방고딕 Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 47);
+            this.label1.Size = new System.Drawing.Size(87, 32);
             this.label1.TabIndex = 13;
             this.label1.Text = "출고처";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // FoodOutput_Process
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1049, 700);
+            this.ClientSize = new System.Drawing.Size(734, 467);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.dataGridView1);
@@ -206,7 +193,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FoodOutput_Process";
             this.Text = "FoodOutput_Process";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
